9a972c998837df4faf72fb21e8a9b87eb0456be5
Sean Ervinson <ervinsonong@gmail.com>
2020-02-24 20:51:07 +0800

    Added login bloc

---
+++ lib/src/components/login/bloc/login_bloc.dart
@@ -0,0 +1,42 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+import 'package:stohp_driver_app/src/components/common/bloc/authentication_bloc.dart';
+import 'package:stohp_driver_app/src/repository/user_repository.dart';
+
+part 'login_event.dart';
+part 'login_state.dart';
+
+class LoginBloc extends Bloc<LoginEvent, LoginState> {
+  final UserRepository userRepository;
+  final AuthenticationBloc authenticationBloc;
+
+  LoginBloc({
+    @required this.userRepository,
+    @required this.authenticationBloc,
+  })  : assert(userRepository != null),
+        assert(authenticationBloc != null);
+
+  LoginState get initialState => LoginInitial();
+
+  @override
+  Stream<LoginState> mapEventToState(LoginEvent event) async* {
+    if (event is LoginButtonPressed) {
+      yield LoginLoading();
+
+      try {
+        final token = await userRepository.authenticate(
+          username: event.username,
+          password: event.password,
+        );
+
+        authenticationBloc.add(LoggedIn(token: token));
+        yield LoginInitial();
+      } catch (error) {
+        yield LoginFailure(error: error.toString());
+      }
+    }
+  }
+}
