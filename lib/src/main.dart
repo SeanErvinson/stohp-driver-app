76e63c27b4e93f7dcd36ed21a3818b8e9df42fac
Sean Ervinson <ervinsonong@gmail.com>
2020-02-24 22:19:31 +0800

    Added authentication bloc

--- lib/src/main.dart
+++ lib/src/main.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:stohp_driver_app/src/repository/user_repository.dart';
 import 'package:stohp_driver_app/src/screens/screens.dart';
 import 'package:stohp_driver_app/src/values/values.dart';
 
-void main() => runApp(MyApp());
+import 'components/common/DefualtBlocDelegate.dart';
+import 'components/common/bloc/authentication_bloc.dart';
 
-class MyApp extends StatelessWidget {
+void main() {
+  BlocSupervisor.delegate = DefaultBlocDelegate();
+  final userRepository = UserRepository();
+  runApp(
+    BlocProvider<AuthenticationBloc>(
+      create: (context) {
+        return AuthenticationBloc(userRepository: userRepository)
+          ..add(AppStarted());
+      },
+      child: StohpDriverApp(userRepository: userRepository),
+    ),
+  );
+}
+
+class StohpDriverApp extends StatelessWidget {
+  final UserRepository userRepository;
+
+  StohpDriverApp({Key key, @required this.userRepository}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,13 +35,32 @@ class MyApp extends StatelessWidget {
       ),
       routes: {
         "welcome": (context) => WelcomeScreen(),
-        "login": (context) => LoginScreen(),
+        "login": (context) => LoginScreen(
+              userRepository: userRepository,
+            ),
         "registration": (context) => RegistrationScreen(),
         "setting": (context) => SettingScreen(),
         "home": (context) => HomeScreen(),
         "splash": (context) => SplashScreen(),
       },
-      home: HomeScreen()
+      home: BlocBuilder<AuthenticationBloc, AuthenticationState>(
+        builder: (context, state) {
+          if (state is Uninitialized) {
+            return SplashScreen();
+          }
+          if (state is Authenticated) {
+            return HomeScreen(
+              user: state.user,
+            );
+          }
+          if (state is Unauthenticated) {
+            return LoginScreen(userRepository: userRepository);
+          }
+          if (state is AuthenticationLoading) {
+            return CircularProgressIndicator();
+          }
+        },
+      ),
     );
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:stohp_driver_app/src/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(StohpDriverApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
