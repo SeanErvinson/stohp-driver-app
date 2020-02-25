159726a3e1aee5baac2e04ee236da6c5e1fefbe2
Sean Ervinson <ervinsonong@gmail.com>
2020-02-25 13:18:28 +0800

    Added login screen

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -1,10 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:stohp_driver_app/src/components/common/bloc/authentication_bloc.dart';
+import 'package:stohp_driver_app/src/components/login/bloc/login_bloc.dart';
+import 'package:stohp_driver_app/src/components/login/form.dart';
+import 'package:stohp_driver_app/src/repository/user_repository.dart';
+import 'package:stohp_driver_app/src/values/values.dart';
 
 class LoginScreen extends StatelessWidget {
+  final UserRepository userRepository;
+
+  LoginScreen({Key key, @required this.userRepository})
+      : assert(userRepository != null),
+        super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-
+    return SafeArea(
+      child: Scaffold(
+        body: BlocProvider<LoginBloc>(
+          create: (context) => LoginBloc(
+            userRepository: userRepository,
+            authenticationBloc: BlocProvider.of<AuthenticationBloc>(context),
+          ),
+          child: Column(
+            children: <Widget>[
+              Container(
+                child: Row(
+                  children: <Widget>[
+                    IconButton(
+                      icon: Icon(Icons.arrow_back),
+                      onPressed: () => Navigator.of(context).pop(),
+                      iconSize: 20,
+                      color: Colors.black54,
+                    ),
+                    Text(
+                      Strings.login,
+                      style: navigationDarkTitle,
+                    ),
+                  ],
+                ),
+              ),
+              LoginForm(),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
