9fcf8ec29c86981d97d6826055cd542955c44bb8
Sean Ervinson <ervinsonong@gmail.com>
2020-02-25 17:17:24 +0800

    Removed unused styles

--- lib/src/values/styles.dart
+++ lib/src/values/styles.dart
@@ -2,28 +2,48 @@ import 'package:flutter/material.dart';
 
 import 'colors.dart';
 
-const TextStyle title = TextStyle(
-  fontSize: 28.0,
-  color: bluePrimary,
+const TextStyle primaryBaseText = TextStyle(
+  color: Colors.black87,
+  fontSize: 16,
+);
+
+const TextStyle primaryBaseBoldText = TextStyle(
+  color: Colors.black87,
+  fontSize: 16,
   fontWeight: FontWeight.bold,
 );
 
-const TextStyle subtitle = TextStyle(
-  fontSize: 14.0,
-  color: Colors.black,
+const TextStyle primaryAppText = TextStyle(
+  color: colorPrimary,
+  fontSize: 16,
+);
+
+const TextStyle primaryAppBoldText = TextStyle(
+  color: colorPrimary,
+  fontSize: 16,
   fontWeight: FontWeight.bold,
 );
 
-const TextStyle button = TextStyle(
-  fontSize: 24.0,
-  fontWeight: FontWeight.bold,
-  color: Colors.white,
+const TextStyle secondaryAppText = TextStyle(
+  color: colorSecondary,
+  fontSize: 16,
 );
 
-const TextStyle subHead = TextStyle(
-  fontSize: 16.0,
-  fontWeight: FontWeight.w400,
-  color: Colors.black,
+const TextStyle secondaryAppBoldText = TextStyle(
+  color: colorSecondary,
+  fontSize: 16,
+  fontWeight: FontWeight.bold,
+);
+
+const TextStyle secondaryBaseText = TextStyle(
+  color: Colors.black54,
+  fontSize: 16,
+);
+
+const TextStyle secondaryBaseBoldText = TextStyle(
+  color: Colors.black54,
+  fontSize: 16,
+  fontWeight: FontWeight.bold,
 );
 
 const TextStyle navigationTitle = TextStyle(
@@ -35,20 +55,4 @@ const TextStyle navigationDarkTitle = TextStyle(
   fontSize: 10.0,
   color: Colors.black,
   fontWeight: FontWeight.bold,
-);
-
-const TextStyle activityInfo = TextStyle(
-  fontSize: 12.0,
-  color: Colors.black87,
-);
-
-const TextStyle cardHeader = TextStyle(
-  color: darkBlue,
-  fontWeight: FontWeight.bold,
-  fontSize: 14.0,
-);
-
-const TextStyle optionButton = TextStyle(
-  color: Colors.white,
-  fontSize: 14.0,
 );
