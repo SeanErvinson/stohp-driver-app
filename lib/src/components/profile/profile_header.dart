10f25ea592886006ba4a25c99e324b8e18090c90
Sean Ervinson <ervinsonong@gmail.com>
2020-02-29 22:47:16 +0800

    Added default image

--- lib/src/components/profile/profile_header.dart
+++ lib/src/components/profile/profile_header.dart
@@ -3,6 +3,8 @@ import 'package:stohp_driver_app/src/models/user.dart';
 import 'package:stohp_driver_app/src/services/api_service.dart';
 
 class ProfileHeader extends StatelessWidget {
+  static const String _defaultProfilePic =
+      "assets/images/default-profile-pic.png";
   const ProfileHeader({
     Key key,
     @required this.user,
@@ -19,8 +21,9 @@ class ProfileHeader extends StatelessWidget {
             padding: const EdgeInsets.only(right: 8.0, left: 8.0, bottom: 8.0),
             child: CircleAvatar(
               maxRadius: 28,
-              backgroundImage:
-                  NetworkImage(ApiService.baseUrl + user.profile.avatar),
+              backgroundImage: user.profile.avatar != null
+                  ? NetworkImage(ApiService.baseUrl + user.profile.avatar)
+                  : AssetImage(_defaultProfilePic),
             ),
           ),
           Text(
