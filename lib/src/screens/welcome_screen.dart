0c096a4f0ae60cba828f5311962f4a3d0d93b955
Sean Ervinson <ervinsonong@gmail.com>
2020-02-29 22:19:48 +0800

    Implemented welcome screen

--- lib/src/screens/welcome_screen.dart
+++ lib/src/screens/welcome_screen.dart
@@ -1,10 +1,87 @@
 import 'package:flutter/material.dart';
+import 'package:stohp_driver_app/src/values/values.dart';
 
 class WelcomeScreen extends StatelessWidget {
+  static const String _backgroundImage = "assets/images/background.jpg";
+  static const String _foregroundImage =
+      "assets/icons/logo-banner-foreground.png";
+  @override
+  Widget build(BuildContext context) {
+    final _usableScreenHeight = MediaQuery.of(context).size.height;
+    final _usableScreenWidth = MediaQuery.of(context).size.width;
+    final _logoSize = 192.0;
+
+    return SafeArea(
+      child: Scaffold(
+        body: Stack(
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  colorFilter: ColorFilter.mode(bgSecondary, BlendMode.dst),
+                  image: AssetImage(_backgroundImage),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Positioned(
+                child: Image.asset(_foregroundImage, width: _logoSize),
+                top: _usableScreenHeight * .1,
+                left: (_usableScreenWidth * .5) - (_logoSize * .5)),
+            Positioned(
+              width: _usableScreenWidth,
+              bottom: 0,
+              child: Container(
+                padding: const EdgeInsets.all(8),
+                child: Column(
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        Flexible(
+                          flex: 1,
+                          child: CredentialButton(
+                            title: Strings.login,
+                            onPressed: () =>
+                                Navigator.pushNamed(context, "login"),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CredentialButton extends StatelessWidget {
+  final String _title;
+  final VoidCallback _onPressed;
+
+  const CredentialButton({Key key, String title, VoidCallback onPressed})
+      : this._onPressed = onPressed,
+        this._title = title,
+        super(key: key);
   @override
   Widget build(BuildContext context) {
     return Container(
-
+      width: double.infinity,
+      child: FlatButton(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(4.0),
+          ),
+          color: Color.fromRGBO(40, 40, 40, .8),
+          child: Text(
+            _title,
+            textAlign: TextAlign.center,
+            style: navigationTitle,
+          ),
+          onPressed: _onPressed),
     );
   }
 }
