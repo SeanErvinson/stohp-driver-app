c400386d246b8ee43caee07389d6627867181437
Sean Ervinson <ervinsonong@gmail.com>
2020-02-29 22:47:03 +0800

    Updated login bloc

--- lib/src/components/login/bloc/login_bloc.dart
+++ lib/src/components/login/bloc/login_bloc.dart
@@ -1,42 +1,81 @@
 import 'dart:async';
-
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-import 'package:meta/meta.dart';
-import 'package:stohp_driver_app/src/components/common/bloc/authentication_bloc.dart';
+import 'package:flutter/widgets.dart';
+import 'package:rxdart/rxdart.dart';
 import 'package:stohp_driver_app/src/repository/user_repository.dart';
+import 'package:stohp_driver_app/src/validation/validation.dart';
 
 part 'login_event.dart';
 part 'login_state.dart';
 
 class LoginBloc extends Bloc<LoginEvent, LoginState> {
-  final UserRepository userRepository;
-  final AuthenticationBloc authenticationBloc;
+  UserRepository _userRepository;
 
-  LoginBloc({
-    @required this.userRepository,
-    @required this.authenticationBloc,
-  })  : assert(userRepository != null),
-        assert(authenticationBloc != null);
+  @override
+  LoginState get initialState => LoginState.empty();
 
-  LoginState get initialState => LoginInitial();
+  LoginBloc({@required UserRepository userRepository})
+      : assert(userRepository != null),
+        _userRepository = userRepository;
+
+  @override
+  Stream<LoginState> transformEvents(
+    Stream<LoginEvent> events,
+    Stream<LoginState> Function(LoginEvent event) next,
+  ) {
+    final nonDebounceStream = events.where((event) {
+      return (event is! OnUsernameChanged && event is! OnPasswordChanged);
+    });
+    final debounceStream = events.where((event) {
+      return (event is OnUsernameChanged || event is OnPasswordChanged);
+    }).debounceTime(Duration(milliseconds: 300));
+    return super.transformEvents(
+      nonDebounceStream.mergeWith([debounceStream]),
+      next,
+    );
+  }
 
   @override
   Stream<LoginState> mapEventToState(LoginEvent event) async* {
-    if (event is LoginButtonPressed) {
-      yield LoginLoading();
+    if (event is OnUsernameChanged) {
+      yield* _mapUsernameChangedToState(event.username);
+    } else if (event is OnPasswordChanged) {
+      yield* _mapPasswordChangedToState(event.password);
+    } else if (event is LoginWithCredentialsPressed) {
+      yield* _mapLoginWithCredentialsPressedToState(
+        username: event.username,
+        password: event.password,
+      );
+    }
+  }
 
-      try {
-        final token = await userRepository.authenticate(
-          username: event.username,
-          password: event.password,
-        );
+  Stream<LoginState> _mapUsernameChangedToState(String username) async* {
+    yield state.update(
+      isUsernameValid: Validators.isValidUsername(username),
+    );
+  }
 
-        authenticationBloc.add(LoggedIn(token: token));
-        yield LoginInitial();
-      } catch (error) {
-        yield LoginFailure(error: error.toString());
-      }
+  Stream<LoginState> _mapPasswordChangedToState(String password) async* {
+    yield state.update(
+      isPasswordValid: Validators.isValidPassword(password),
+    );
+  }
+
+  Stream<LoginState> _mapLoginWithCredentialsPressedToState({
+    String username,
+    String password,
+  }) async* {
+    yield LoginState.loading();
+    try {
+      final token = await _userRepository.authenticate(
+        username: username,
+        password: password,
+      );
+      await _userRepository.persistToken(token);
+      yield LoginState.success();
+    } catch (_) {
+      yield LoginState.failure();
     }
   }
 }
--- lib/src/components/login/bloc/login_event.dart
+++ lib/src/components/login/bloc/login_event.dart
@@ -2,13 +2,40 @@ part of 'login_bloc.dart';
 
 abstract class LoginEvent extends Equatable {
   const LoginEvent();
+
+  @override
+  List<Object> get props => [];
 }
 
-class LoginButtonPressed extends LoginEvent {
+class OnUsernameChanged extends LoginEvent {
+  final String username;
+
+  const OnUsernameChanged({@required this.username});
+
+  @override
+  List<Object> get props => [username];
+
+  @override
+  String toString() => 'UsernameChanged { email :$username }';
+}
+
+class OnPasswordChanged extends LoginEvent {
+  final String password;
+
+  const OnPasswordChanged({@required this.password});
+
+  @override
+  List<Object> get props => [password];
+
+  @override
+  String toString() => 'PasswordChanged { password :$password }';
+}
+
+class OnSumbitted extends LoginEvent {
   final String username;
   final String password;
 
-  const LoginButtonPressed({
+  const OnSumbitted({
     @required this.username,
     @required this.password,
   });
@@ -17,6 +44,27 @@ class LoginButtonPressed extends LoginEvent {
   List<Object> get props => [username, password];
 
   @override
-  String toString() =>
-      'LoginButtonPressed { username: $username, password: $password }';
+  String toString() {
+    return 'Submitted { email: $username, password: $password }';
+  }
+}
+
+class LoginWithGooglePressed extends LoginEvent {}
+
+class LoginWithCredentialsPressed extends LoginEvent {
+  final String username;
+  final String password;
+
+  const LoginWithCredentialsPressed({
+    @required this.username,
+    @required this.password,
+  });
+
+  @override
+  List<Object> get props => [username, password];
+
+  @override
+  String toString() {
+    return 'LoginWithCredentialsPressed { email: $username, password: $password }';
+  }
 }
