eb4d66cf81ff6f61f02526c271034cdd8395277f
Sean Ervinson <ervinsonong@gmail.com>
2020-02-29 22:47:38 +0800

    Standardized app bar

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -1,45 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:stohp_driver_app/src/components/common/bloc/authentication_bloc.dart';
 import 'package:stohp_driver_app/src/components/login/bloc/login_bloc.dart';
 import 'package:stohp_driver_app/src/components/login/form.dart';
 import 'package:stohp_driver_app/src/repository/user_repository.dart';
 import 'package:stohp_driver_app/src/values/values.dart';
 
 class LoginScreen extends StatelessWidget {
-  final UserRepository userRepository;
+  final UserRepository _userRepository;
 
-  LoginScreen({Key key, @required this.userRepository})
+  LoginScreen({Key key, @required UserRepository userRepository})
       : assert(userRepository != null),
+        _userRepository = userRepository,
         super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: BlocProvider<LoginBloc>(
-          create: (context) => LoginBloc(
-            userRepository: userRepository,
-            authenticationBloc: BlocProvider.of<AuthenticationBloc>(context),
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(48),
+          child: AppBar(
+            titleSpacing: 0,
+            backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+            elevation: 0,
+            leading: IconButton(
+              icon: Icon(Icons.arrow_back),
+              onPressed: () => Navigator.of(context).pop(),
+              color: Colors.black87,
+            ),
+            title: Text(
+              Strings.login,
+              style: TextStyle(color: Colors.black87, fontSize: 14),
+            ),
           ),
+        ),
+        body: BlocProvider<LoginBloc>(
+          create: (context) => LoginBloc(userRepository: _userRepository),
           child: Column(
             children: <Widget>[
-              Container(
-                child: Row(
-                  children: <Widget>[
-                    IconButton(
-                      icon: Icon(Icons.arrow_back),
-                      onPressed: () => Navigator.of(context).pop(),
-                      iconSize: 20,
-                      color: Colors.black54,
-                    ),
-                    Text(
-                      Strings.login,
-                      style: navigationDarkTitle,
-                    ),
-                  ],
-                ),
-              ),
               LoginForm(),
             ],
           ),
