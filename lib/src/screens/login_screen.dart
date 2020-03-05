6ca5c5aa360ce13d4aa3a03466c4b4839811dda7
Sean Ervinson <ervinsonong@gmail.com>
2020-03-05 22:38:29 +0800

    Added scrollview for form

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -7,6 +7,7 @@ import 'package:stohp_driver_app/src/values/values.dart';
 
 class LoginScreen extends StatelessWidget {
   final UserRepository _userRepository;
+  static const double appBarHeight = 48.0;
 
   LoginScreen({Key key, @required UserRepository userRepository})
       : assert(userRepository != null),
@@ -15,6 +16,8 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final _usableScreenHeight =
+        MediaQuery.of(context).size.height - MediaQuery.of(context).padding.top;
     return SafeArea(
       child: Scaffold(
         backgroundColor: Theme.of(context).scaffoldBackgroundColor,
@@ -29,18 +32,32 @@ class LoginScreen extends StatelessWidget {
               onPressed: () => Navigator.of(context).pop(),
               color: Colors.black87,
             ),
-            title: Text(
-              Strings.login,
-              style: TextStyle(color: Colors.black87, fontSize: 14),
-            ),
           ),
         ),
-        body: BlocProvider<LoginBloc>(
-          create: (context) => LoginBloc(userRepository: _userRepository),
-          child: Column(
-            children: <Widget>[
-              LoginForm(),
-            ],
+        body: SingleChildScrollView(
+          child: Container(
+            height: MediaQuery.of(context).size.height -
+                appBarHeight -
+                MediaQuery.of(context).padding.top,
+            child: BlocProvider<LoginBloc>(
+              create: (context) => LoginBloc(userRepository: _userRepository),
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    height: _usableScreenHeight * .15,
+                    alignment: Alignment.topCenter,
+                    child: Text(
+                      Strings.loginHeader.toUpperCase(),
+                      style: TextStyle(
+                          color: colorSecondary,
+                          fontSize: 24.0,
+                          fontWeight: FontWeight.w300),
+                    ),
+                  ),
+                  LoginForm()
+                ],
+              ),
+            ),
           ),
         ),
       ),
