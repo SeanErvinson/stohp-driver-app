dbbf83cc43e2a0555873d036b6f6f1c912cb3e92
Sean Ervinson <ervinsonong@gmail.com>
2020-03-06 23:50:34 +0800

    Added animation to login button

--- lib/src/screens/welcome_screen.dart
+++ lib/src/screens/welcome_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:simple_animations/simple_animations.dart';
 import 'package:stohp_driver_app/src/values/values.dart';
 
 class WelcomeScreen extends StatelessWidget {
@@ -15,19 +16,12 @@ class WelcomeScreen extends StatelessWidget {
       child: Scaffold(
         body: Stack(
           children: <Widget>[
-            Container(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  colorFilter: ColorFilter.mode(bgSecondary, BlendMode.dst),
-                  image: AssetImage(_backgroundImage),
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ),
+            BackgroundImageContainer(backgroundImage: _backgroundImage),
             Positioned(
-                child: Image.asset(_foregroundImage, width: _logoSize),
-                top: _usableScreenHeight * .1,
-                left: (_usableScreenWidth * .5) - (_logoSize * .5)),
+              child: Image.asset(_foregroundImage, width: _logoSize),
+              top: _usableScreenHeight * .1,
+              left: (_usableScreenWidth * .5) - (_logoSize * .5),
+            ),
             Positioned(
               width: _usableScreenWidth,
               bottom: 0,
@@ -40,10 +34,13 @@ class WelcomeScreen extends StatelessWidget {
                       children: <Widget>[
                         Flexible(
                           flex: 1,
-                          child: CredentialButton(
-                            title: Strings.login,
-                            onPressed: () =>
-                                Navigator.pushNamed(context, "login"),
+                          child: FadeIn(
+                            4.2,
+                            CredentialButton(
+                              title: Strings.login,
+                              onPressed: () =>
+                                  Navigator.pushNamed(context, "login"),
+                            ),
                           ),
                         ),
                       ],
@@ -54,6 +51,62 @@ class WelcomeScreen extends StatelessWidget {
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class BackgroundImageContainer extends StatelessWidget {
+  const BackgroundImageContainer({
+    Key key,
+    @required String backgroundImage,
+  })  : _backgroundImage = backgroundImage,
+        super(key: key);
+
+  final String _backgroundImage;
+
+  @override
+  Widget build(BuildContext context) {
+    final _usableScreenHeight =
+        MediaQuery.of(context).size.height - MediaQuery.of(context).padding.top;
+    return Container(
+      height: _usableScreenHeight,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          colorFilter: ColorFilter.mode(bgSecondary, BlendMode.dst),
+          image: AssetImage(_backgroundImage),
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
+
+class FadeIn extends StatelessWidget {
+  final double delay;
+  final Widget child;
+
+  FadeIn(this.delay, this.child);
+
+  @override
+  Widget build(BuildContext context) {
+    final tween = MultiTrackTween([
+      Track("opacity")
+          .add(Duration(milliseconds: 500), Tween(begin: 0.0, end: 1.0)),
+      Track("translateY").add(
+          Duration(milliseconds: 500), Tween(begin: 24.0, end: 0.0),
+          curve: Curves.easeOut)
+    ]);
+
+    return ControlledAnimation(
+      delay: Duration(milliseconds: (300 * delay).round()),
+      duration: tween.duration,
+      tween: tween,
+      child: child,
+      builderWithChild: (context, child, animation) => Opacity(
+        opacity: animation["opacity"],
+        child: Transform.translate(
+            offset: Offset(0, animation["translateY"]), child: child),
       ),
     );
   }
