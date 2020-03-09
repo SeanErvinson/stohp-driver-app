b9fa763c9d446ea4e1b48a024049216547f5553e
Sean Ervinson <ervinsonong@gmail.com>
2020-03-09 22:57:16 +0800

    Included filter from model

--- lib/src/models/commuter_oversight_info.dart
+++ lib/src/models/commuter_oversight_info.dart
@@ -1,14 +1,18 @@
 class CommuterOversightInfo {
   String id;
+  String vehicleType;
+  String route;
   double lat;
   double lng;
 
-  CommuterOversightInfo({this.id, this.lat, this.lng});
+  CommuterOversightInfo({this.id, this.lat, this.lng, this.route, this.vehicleType});
 
   CommuterOversightInfo.fromJson(Map<String, dynamic> json) {
     id = json['id'];
     lat = json['lat'];
     lng = json['lng'];
+    vehicleType = json['vehicle_type'];
+    route = json['route'];
   }
 
   Map<String, dynamic> toJson() {
@@ -16,6 +20,8 @@ class CommuterOversightInfo {
     data['id'] = this.id;
     data['lat'] = this.lat;
     data['lng'] = this.lng;
+    data['route'] = this.route;
+    data['vehicle_type'] = this.vehicleType;
     return data;
   }
 }
