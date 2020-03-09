1263645325d3fd4b35e8718edfc40d17d5d022d4
Sean Ervinson <ervinsonong@gmail.com>
2020-03-10 00:14:08 +0800

    Added on off location update

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:stohp_driver_app/src/components/common/bloc/greet_bloc.dart';
 import 'package:stohp_driver_app/src/components/common/stohp_driver_icons.dart';
 import 'package:stohp_driver_app/src/components/common/stop_code_argument.dart';
+import 'package:stohp_driver_app/src/components/home/bloc/location_update_bloc.dart';
 import 'package:stohp_driver_app/src/components/home/bloc/oversight_bloc.dart';
 import 'package:stohp_driver_app/src/components/home/bloc/space_bloc.dart';
 import 'package:stohp_driver_app/src/components/home/oversight_map.dart';
@@ -107,48 +108,87 @@ class HomeScreen extends StatelessWidget {
             body: Column(
               children: <Widget>[
                 Container(
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      colors: headerColor,
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter,
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: headerColor,
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                      ),
                     ),
-                  ),
-                  alignment: Alignment.center,
-                  width: double.infinity,
-                  height: _usableScreenHeight * .08,
-                  child: BlocBuilder<GreetBloc, GreetState>(
-                    bloc: BlocProvider.of<GreetBloc>(context),
-                    builder: (context, state) {
-                      String greetings = Strings.greetingsDefault;
-                      if (state is GreetMorning) {
-                        greetings = Strings.greetingsMorning;
-                      }
-                      if (state is GreetAfternoon) {
-                        greetings = Strings.greetingsAfternoon;
-                      }
-                      if (state is GreetEvening) {
-                        greetings = Strings.greetingsEvening;
-                      }
-                      return RichText(
-                        textAlign: TextAlign.center,
-                        text: TextSpan(
-                            text: greetings,
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 24.0),
-                            children: [
-                              TextSpan(text: "\n"),
-                              TextSpan(
-                                text: "${user.firstName}",
-                                style: TextStyle(
-                                    fontWeight: FontWeight.w400,
-                                    fontSize: 18.0),
-                              ),
-                            ]),
-                      );
-                    },
-                  ),
-                ),
+                    alignment: Alignment.center,
+                    width: double.infinity,
+                    height: _usableScreenHeight * .08,
+                    child: Stack(
+                      children: <Widget>[
+                        Align(
+                          alignment: Alignment.center,
+                          child: BlocBuilder<GreetBloc, GreetState>(
+                            bloc: BlocProvider.of<GreetBloc>(context),
+                            builder: (context, state) {
+                              String greetings = Strings.greetingsDefault;
+                              if (state is GreetMorning) {
+                                greetings = Strings.greetingsMorning;
+                              }
+                              if (state is GreetAfternoon) {
+                                greetings = Strings.greetingsAfternoon;
+                              }
+                              if (state is GreetEvening) {
+                                greetings = Strings.greetingsEvening;
+                              }
+                              return RichText(
+                                textAlign: TextAlign.center,
+                                text: TextSpan(
+                                    text: greetings,
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 24.0),
+                                    children: [
+                                      TextSpan(text: "\n"),
+                                      TextSpan(
+                                        text: "${user.firstName}",
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.w400,
+                                            fontSize: 18.0),
+                                      ),
+                                    ]),
+                              );
+                            },
+                          ),
+                        ),
+                        Align(
+                          alignment: Alignment.centerRight,
+                          child: BlocBuilder<LocationUpdateBloc,
+                              LocationUpdateState>(
+                            bloc: BlocProvider.of<LocationUpdateBloc>(context),
+                            builder: (context, state) {
+                              IconData icon;
+                              Color color;
+                              VoidCallback onPressed;
+                              if (state is LocationTurnOffUpdate) {
+                                icon = Icons.power_settings_new;
+                                color = Colors.grey;
+                                onPressed = () =>
+                                    BlocProvider.of<LocationUpdateBloc>(context)
+                                        .add(TurnOnUpdate());
+                              } else if (state is LocationTurnOnUpdate) {
+                                icon = Icons.power_settings_new;
+                                color = Colors.white;
+                                onPressed = () =>
+                                    BlocProvider.of<LocationUpdateBloc>(context)
+                                        .add(TurnOffUpdate());
+                              }
+                              return IconButton(
+                                icon: Icon(
+                                  icon,
+                                  color: color,
+                                ),
+                                onPressed: onPressed,
+                              );
+                            },
+                          ),
+                        )
+                      ],
+                    )),
                 Expanded(child: OversightMap())
               ],
             ),
